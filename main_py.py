7a4b5a20eaf6ecc42ef03cd121546dd35e4e27af
KimJongha <wkdghk918@gmail.com>
2019-12-04 15:50:03 +0900

    Othello_project_01

--- main_py.py
+++ main_py.py
@@ -1,6 +1,22 @@
-#커밋 연습
-a = int(input("Input a :"))
-print("Print a :\n",a)
-while a < 5 :
-    a+=1
-    print("Add 1 to value a\n--> ",a)
+#Othel
+def show_board(board):
+    k_1=['①','②','③','④','⑤','⑥','⑦','⑧']
+    i=0
+    print()
+    print('*','|','①','②','③','④','⑤','⑥','⑦','⑧')
+    print('*','+','-','-','-','-','-','-','-','-')
+    while i<8:
+	    print(k_1[i],'|',board[i][0],board[i][1],board[i][2]\
+				,board[i][3],board[i][4],board[i][5],board[i][6]\
+				,board[i][7])
+	    i+=1
+    print()
+def create_board():#creating board array
+    board=[[],[],[],[],[],[],[],[]]
+    for i in range(0,8):
+        for j in range(0,8):
+             board[i].append('ㆍ')
+    return board
+board = create_board()
+show_board(board)
+
