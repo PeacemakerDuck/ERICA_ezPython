4f6f39b7e6b3f8a6a23c31ac4403387ed7fe93bb
KimJongha <wkdghk918@gmail.com>
2019-12-05 15:14:38 +0900

    othello_02

--- main_py.py
+++ main_py.py
@@ -1,10 +1,10 @@
-#Othel
+#Othello
 def show_board(board):
     k_1=['①','②','③','④','⑤','⑥','⑦','⑧']
     i=0
     print()
-    print('*','|','①','②','③','④','⑤','⑥','⑦','⑧')
-    print('*','+','-','-','-','-','-','-','-','-')
+    print('*','|','①',' ②',' ③',' ④',' ⑤',' ⑥',' ⑦',' ⑧')
+    print('-','+','-','-','-','-','-','-','-','-','-','-','-','-')
     while i<8:
 	    print(k_1[i],'|',board[i][0],board[i][1],board[i][2]\
 				,board[i][3],board[i][4],board[i][5],board[i][6]\
@@ -17,7 +17,31 @@ def create_board():#creating board array
         for j in range(0,8):
              board[i].append('ㆍ')
     return board
-def orthello():
-	pass
-board = create_board()
-show_board(board)
+def cordinate_board(message,i,j):#receiving cordinate
+    while True:
+        try :
+            number = input(message+' ')
+            while not int(number) in range(i,j+1): 
+                number = input('\nType the number in range.\n\n'+ message)
+            break
+        except ValueError:
+            print('\nType in the Integer value.\n\n')
+        except KeyboardInterrupt:
+            choice=input('\nSurrender(y/n):')
+            if choice=='y':
+                print('\nSurrender\n')
+                return -1
+            else:
+                continue
+    return int(number)
+def surrender(num):
+    if num == -1:
+	    print("surrender")
+def othello():#surrender, score check, turn change, setting piece
+	board = create_board()
+	show_board(board)
+	cord_x = cordinate_board("row number(1~8):",1,8)
+	surrender(cord_x)
+	cord_y = cordinate_board("column number(1~8):",1,8)
+	surrender(cord_y)
+othello()
